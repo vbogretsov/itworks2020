a7a52703b9f743b3601977d98ad3b32283b30552
Bogretsov <bogrecov@gmail.com>
2019-11-24 23:55:25 +0300

    Added infrastructure

---
+++ infra/iam/main.tf
@@ -0,0 +1,252 @@
+provider "aws" {}
+
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    workspace_key_prefix = ""
+    key                  = "itworks2020-iam.tfstate"
+  }
+
+  required_providers {
+    aws = "= 2.36.0"
+  }
+}
+
+locals {
+  bucket     = "${var.prefix}-${replace(var.domain, ".", "-")}"
+  region     = data.aws_region.current.name
+  account_id = data.aws_caller_identity.current.account_id
+  statename  = "itworks2020-res.tfstate"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_route53_zone" "this" {
+  name         = "${var.domain}."
+  private_zone = false
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.tfstate}/*/${local.statename}",
+      "arn:aws:s3:::${var.tfstate}/${local.statename}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.tfstate}",
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:CreateBucket",
+      "s3:DeleteBucket",
+      "s3:ListBucket",
+      "s3:GetBucket*",
+      "s3:GetAccountPublicAccessBlock",
+      "s3:Get*Configuration",
+      "s3:PutBucketPolicy",
+      "s3:DeleteBucketPolicy",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket}"
+    ]
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket}/*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudfront:*CloudFrontOriginAccessIdentity*",
+    ]
+
+    resources = [
+      # "arn:aws:cloudfront::${local.account_id}:origin-access-identity/*"
+      "*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudfront:CreateDistribution",
+      "cloudfront:DeleteDistribution",
+      "cloudfront:GetDistribution",
+      "cloudfront:GetDistributionConfig",
+      "cloudfront:UpdateDistribution",
+      "cloudfront:ListTagsForResource",
+      "cloudfront:*agResource",
+    ]
+
+    resources = [
+      "arn:aws:cloudfront::${local.account_id}:distribution/*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "cloudfront:ListDistributions",
+      "cloudfront:ListCloudFrontOriginAccessIdentities",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "route53:ListHostedZones",
+      "route53:ListHostedZonesByName",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "route53:GetChange",
+    ]
+
+    resources = [
+      "arn:aws:route53:::change/*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "route53:GetHostedZone",
+      "route53:*RecordSets",
+    ]
+
+    resources = [
+      "arn:aws:route53:::hostedzone/${data.aws_route53_zone.this.id}"
+    ]
+  }
+
+  statement {
+    actions = [
+      "acm:DeleteCertificate",
+      "acm:DescribeCertificate",
+      "acm:GetCertificate",
+      "acm:UpdateCertificateOptions",
+      "acm:ListTagsForCertificate",
+    ]
+
+    resources = [
+      "arn:aws:acm:${local.region}:${local.account_id}:certificate/*",
+      "arn:aws:acm:us-east-1:${local.account_id}:certificate/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "acm:RequestCertificate",
+      "acm:ListCertificates",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    actions = [
+      "ec2:DescribeAccountAttributes",
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "arn:aws:iam::${local.account_id}:root"
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "group" {
+  statement {
+    actions   = ["sts:AssumeRole"]
+    resources = [aws_iam_role.this.arn]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = var.prefix
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+resource "aws_iam_role_policy" "this" {
+  name   = var.prefix
+  role   = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_group" "this" {
+  name = var.prefix
+}
+
+resource "aws_iam_group_policy" "this" {
+  name   = var.prefix
+  group  = aws_iam_group.this.id
+  policy = data.aws_iam_policy_document.group.json
+}
+
+resource "aws_iam_user" "this" {
+  name = "${var.prefix}-admin"
+}
+
+resource "aws_iam_access_key" "this" {
+  user = aws_iam_user.this.name
+}
+
+resource "aws_iam_user_group_membership" "this" {
+  user   = aws_iam_user.this.name
+  groups = [aws_iam_group.this.name]
+}
+
+resource "local_file" "awsconfig" {
+  content = templatefile("${path.module}/awsconfig", {
+    region            = data.aws_region.current.name
+    access_key_id     = aws_iam_access_key.this.id
+    secret_access_key = aws_iam_access_key.this.secret
+  })
+  filename = "${path.module}/.awsconfig"
+}
---
+++ infra/res/vars.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  type        = string
+  description = "Project prefix."
+}
+
+variable "domain" {
+  type        = string
+  description = "Domain name registered in Route 53."
+}
