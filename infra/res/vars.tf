07fc425e4e45f7846f93af469c23213f287bc81c
Bogretsov <bogrecov@gmail.com>
2019-12-03 23:30:40 +0300

    addded entrypoint

--- infra/res/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "prefix" {
-  type        = string
-  description = "Project prefix."
-}
-
-variable "domain" {
-  type        = string
-  description = "Domain name registered in Route 53."
-}
